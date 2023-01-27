7ae728b793be1065c04e6a97f3752e788abc74b1
ihbhuiyan28 <ihbhuiyan28@gmail.com>
2023-01-27 17:14:39 +0600

    Add Model "Student.cs"

---
+++ StudentApp/StudentApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Registration { get; set; }
+        public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public double Result { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
