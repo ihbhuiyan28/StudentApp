31b06a5bac79f005cd70dfde2f01eb91fbbaa239
ihbhuiyan28 <ihbhuiyan28@gmail.com>
2023-01-27 20:14:44 +0600

    Update "Student.cs"

--- StudentApp/StudentApp/Models/Student.cs
+++ StudentApp/StudentApp/Models/Student.cs
@@ -5,10 +5,17 @@ namespace StudentApp.Models
     public class Student
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Registration is Required")]
+        [RegularExpression(@"^\d{1,11}$", ErrorMessage = "Characters Not Allowed")]
         public string? Registration { get; set; }
+        [Required(ErrorMessage = "Gender is Required")]
         public string? Gender { get; set; }
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "Result is Required")]
         public double Result { get; set; }
     }
 }
